52f5a5b09baccdc19b978a1c72d0e6e2899d7525
emewarp <martawarp@gmail.com>
2020-08-01 14:14:51 +0200

    extract game to component

--- src/components/PlayerButton.js
+++ src/components/PlayerButton.js
@@ -5,7 +5,7 @@ const PlayerButton = props => (
     <button className="gameBox"
       style={{backgroundColor: colors[props.gameBoxState]}}
       onClick={()=> props.onClick(props.player, props.gameBoxKey)}
-      >
+    >
     </button> 
 );
 
