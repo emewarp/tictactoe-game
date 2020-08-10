d513e9b2af06fd4bcae0d81dae530e6dc2e98420
emewarp <martawarp@gmail.com>
2020-08-10 21:45:32 +0200

    style start menu

--- src/components/StartMenu.js
+++ src/components/StartMenu.js
@@ -3,10 +3,13 @@ import React from 'react';
 const StartGame = props => (
     <div className="start-game">
         <div className="start-game-message">
-            play !
+            play game !
         </div>
+        <button className="start-game-button" onClick={props.onPlayGameSolo}>
+            One player
+        </button>
         <button className="start-game-button" onClick={props.onPlayGame}>
-            Start Playing
+            Two players
         </button>
    </div>
 
